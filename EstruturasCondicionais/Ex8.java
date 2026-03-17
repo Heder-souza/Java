54b29f703edb398293aaeaf6b59179271ac985bf
Heder-souza <hederdesouzasilva@gmail.com>
2026-03-17 09:26:38 -0300

    exercicios finalizados até o 7

--- EstruturasCondicionais/Ex8.java
+++ EstruturasCondicionais/Ex8.java
@@ -4,7 +4,7 @@ public class Ex8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Digite seu peso (kg)")
 
         scanner.close()
     }
