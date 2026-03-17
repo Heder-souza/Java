69fde8da16337b3ab39283fae36f45f0c8e12ac0
Heder-souza <hederdesouzasilva@gmail.com>
2026-03-17 09:36:49 -0300

    exercicio completo até o 8

--- EstruturasCondicionais/Ex8.java
+++ EstruturasCondicionais/Ex8.java
@@ -4,8 +4,30 @@ public class Ex8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu peso (kg)")
+        System.out.println("Digite seu peso (kg)");
+        double p = scanner.nextDouble();
 
-        scanner.close()
+        System.out.println("Digite seu altura (m)");
+        double a = scanner.nextDouble();
+
+        double imc = p/(a*a);
+
+        if(imc < 18.5){
+            System.out.println("Abaixo do peso");
+        }
+        else if (imc >= 18.5 && imc < 25){
+            System.out.println("Peso normal");
+        }
+        else if( imc >= 25 && imc < 30){
+            System.out.println("Acima do peso");
+        }
+        else{
+            System.out.println("obesidade");
+        }
+
+        System.out.println((imc >= 18.5 && imc < 25)? "faixa saudavel" : "fora da faixa saudavel");
+
+
+        scanner.close();
     }
 }
