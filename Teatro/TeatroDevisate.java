a3e4eb6cb9c4f9d105ddc087aca0adac269e2c77
Heder-souza <hederdesouzasilva@gmail.com>
2026-03-18 08:44:48 -0300

    inicio do programa Teatro

--- Teatro/TeatroDevisate.java
+++ Teatro/TeatroDevisate.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class TeatroDevisate{
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("============================");
+        System.out.println("       Teatro Devisate");
+        System.out.println("============================");
+        System.out.println("1 - Cadastro da Sala");
+        System.out.println("2 - Exibir Mapa da Sala");
+        System.out.println("3 - Reservar Cadeira");
+        System.out.println("4 - Comprar Cadeira");
+        System.out.println("5 - Cancelar reserva");
+        System.out.println("6 - Relatório Financeiro");
+        System.out.println("0 - Sair");
+        System.out.println("============================");
+
+
+        scanner.close();
     }
 }
